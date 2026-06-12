e42729461480b45785fe9f3310bb3d1ab2391e1d
nickwilde4 <nicolaspwilde4@gmail.com>
2026-06-11 22:16:19 -0500

    bether movement and background

--- tetrix.cpp
+++ tetrix.cpp
@@ -1,35 +1,67 @@
 #include <SFML/Graphics.hpp>
-#include <SFML/Window/Keyboard.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
 
 int main()  {
-
-
-    constexpr unsigned int window_tall = 750, window_large = 1000; //Size of the window
+    int i = 0;
+    bool move = 1;
+    constexpr unsigned int window_tall = 1000, window_large = 1000; //Size of the window
     sf::RenderWindow window (
     sf::VideoMode({window_large, window_tall}),
     "Nick Tetrix_SMLF");
-    struct Balls {
-    float x = 500.f; 
-    float y = 500.f;
-    float radius = 50;
-    sf::CircleShape circle_shape = sf::CircleShape(radius);
-    };
-    Balls ball[1];
-    ball[0].circle_shape.setPosition({ball[0].x,ball[0].y});
 
-    while (window.isOpen()){
+    window.setFramerateLimit(60);
+    struct Objects {
+    float x = 500.f; 
+    float y = 0.f;
+    float side = 50;
+    sf::RectangleShape rectangle_shape = sf::RectangleShape({side,side});
+    };
+    Objects object[1];
+    object[0].rectangle_shape.setPosition({object[0].x,object[0].y});
+
+        sf::RectangleShape partofbackgraund1 = sf::RectangleShape({(window_large/6), window_tall/6});
+        partofbackgraund1.setPosition({20.f+(window_large - (window_large/6)), 15.f});
+
+        sf::RectangleShape backgraund = sf::RectangleShape({(window_large/6),window_tall});
+        backgraund.setPosition({window_large - (window_large/6), 0});
+        backgraund.setFillColor(sf::Color::Blue);
+            
+
+    while (window.isOpen()) {
+    
+    i++;
+
+
+
+
+
+
+
+
+
         while (const auto event = window.pollEvent()){
             if (event->is<sf::Event::Closed>()) window.close();
-             
-                    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) ball[0].y -= 5.f;
-                    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) ball[0].x -= 5.f;
-                    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) ball[0].y += 5.f;
-                    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) ball[0].x += 5.f;
-                    ball[0].circle_shape.setPosition({ball[0].x,ball[0].y});
+            if (move) {
+                if (const auto* press_key = event->getIf<sf::Event::KeyPressed>()) {
+                    if (press_key->code == sf::Keyboard::Key::A) {object[0].x -= 50.f;move=0;}
+                    if (press_key->code == sf::Keyboard::Key::S) {object[0].y += 50.f;move=0;}
+                    if (press_key->code == sf::Keyboard::Key::D) {object[0].x += 50.f;move=0;}
+                }
+            }
         }
+        if (i>=60) {object[0].y += object[0].side; i = 0; move = 1;}
+        
+        
+                if (object[0].y+ (object[0].side) >= window_tall){ object[0].y = window_tall - (object[0].side);}
+                if (object[0].x < 0) object[0].x = 0;
+                if (object[0].x + (object[0].side) > window_large-(window_large/6)) object[0].x = window_large-(window_large/6) - (object[0].side);
+                object[0].rectangle_shape.setPosition({object[0].x,object[0].y});  
     
             window.clear();
-            window.draw(ball[0].circle_shape);
+            window.draw(partofbackgraund1);
+            window.draw(backgraund);
+            window.draw(object[0].rectangle_shape);
             window.display();
-    }        
+    }
 }
